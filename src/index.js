cd19113df737722734fbd4cfaa79826b7c86b43c
MarK22100 <fmzambrana3@gmail.com>
2024-04-24 23:00:52 -0300

    se crear estructura inicial para backend

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const path=require('path');
+const { log } = require('console');
+
+const app=express();
+
+require('dotenv').config();
+
+app.set('port', process.env.PORT || 9001);
+
+app.listen(app.get('port'), ()=>{console.log(`SERVER READY IN PORT ${app.get('port')}`);})
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(morgan('dev'))
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname,'../public')))
